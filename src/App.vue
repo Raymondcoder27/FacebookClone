41f64103202c71bcd5db073b65a548846f737d79
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-02 17:09:35 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/posts">Posts</RouterLink>
+
       </nav>
     </div>
   </header>
