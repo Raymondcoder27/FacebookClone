5b786a0f2aae4b4560e88a8dea8dba32440990f9
MwebeCoder27 <mwebraymond13@gmail.com>
2024-09-02 16:59:50 +0300

    init commit

--- src/stores/general.js
+++ src/stores/general.js
@@ -1,12 +1,13 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useGeneralStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
+export const useGeneralStore = defineStore('general', () => {
+  const isPostOverlay = ref(false)
+  const isCropperModal = ref(false)
+  const isImageDisplay = ref(null)
   function increment() {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  return { isPostOverlay, isCropperModal, isImageDisplay, increment }
 })
