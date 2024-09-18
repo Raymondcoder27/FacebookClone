3d5bf811f7cf5580f2b98e64f4d4007cf438251b
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-18 12:19:06 +0300

    init

--- src/Layouts/MainNavLayout.vue
+++ src/Layouts/MainNavLayout.vue
@@ -1,5 +1,16 @@
 <script setup>
     import {ref} from 'vue'
+    import Magnify from 'vue-material-design-icons/Magnify.vue'
+    import Home from 'vue-material-design-icons/Home.vue'
+    import HomeOutline from 'vue-material-design-icons/HomeOutline.vue'
+    import TelevisionPlay from 'vue-material-design-icons/TelevisionPlay.vue'
+    import StorefrontOutline from 'vue-material-design-icons/StorefrontOutline.vue'
+    import AccountGroup from 'vue-material-design-icons/AccountGroup.vue'
+    import ControllerClassicOutline from 'vue-material-design-icons/ControllerClassicOutline.vue'
+    import DotsGrid from 'vue-material-design-icons/DotsGrid.vue'
+    import FacebookMessanger from 'vue-material-design-icons/FacebookMessanger.vue'
+    import Bell from 'vue-material-design-icons/Bell.vue'
+    import Logout from 'vue-material-design-icons/Logout.vue'
     // import {Link} from '@inertiajs/vue3'
 </script>
 
