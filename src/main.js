811c706bb06cfe78b30e8efeb30b397ac3eeab46
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-18 12:10:35 +0300

    init commit

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import App from './App.vue'
 import router from './router'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+
 const app = createApp(App)
 
 app.use(createPinia())
