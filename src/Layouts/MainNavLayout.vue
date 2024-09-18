50bf5558e8c9b81c495e55e6acf2452d12d3b3aa
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-18 12:23:29 +0300

    init

--- src/Layouts/MainNavLayout.vue
+++ src/Layouts/MainNavLayout.vue
@@ -8,14 +8,23 @@
     import AccountGroup from 'vue-material-design-icons/AccountGroup.vue'
     import ControllerClassicOutline from 'vue-material-design-icons/ControllerClassicOutline.vue'
     import DotsGrid from 'vue-material-design-icons/DotsGrid.vue'
-    import FacebookMessanger from 'vue-material-design-icons/FacebookMessanger.vue'
+    import FacebookMessenger from 'vue-material-design-icons/FacebookMessenger.vue'
     import Bell from 'vue-material-design-icons/Bell.vue'
     import Logout from 'vue-material-design-icons/Logout.vue'
     // import {Link} from '@inertiajs/vue3'
+
+    import {useGeneralStore} from '@/stores/general'
+    import { storeToRefs } from 'pinia'
+    const useGeneral = useGeneralStore()
+    const {isPostOverlay, isCropperModal, isImageDisplay} = storeToRefs(useGeneral)
+
+    const user = usePage().props.auth.user
+
+    let showMenu = ref(false)
 </script>
 
 <template>
-    <div class="w-full bg-red-400 fixed">
+    <div class="w-full fixed">
         Cool
     </div>
 </template>
