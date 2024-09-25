3022eda113e54246adee2e68c36b12738f779468
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-25 14:16:36 +0300

    init

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,21 +1,18 @@
 <script setup>
-import MainNavLayout from '@/Layouts/MainNavLayout.vue';
+import MainNavLayout from "@/Layouts/MainNavLayout.vue";
 
-import Magnify from 'vue-material-design-icons/Magnify.vue';
-import TelevisionPlay from 'vue-material-design-icons/TelevisionPlay.vue';
-import StorefrontOutline from 'vue-material-design-icons/StorefrontOutline.vue';
-import AccountGroup from 'vue-material-design-icons/AccountGroup.vue';
-import AccountMultiple from 'vue-material-design-icons/AccountMultiple.vue';
-import Flag from 'vue-material-design-icons/Flag.vue';
-import ClockTimeTwoOutline from 'vue-material-design-icons/ClockTimeTwoOutline.vue';
-import Restore from 'vue-material-design-icons/Restore.vue';
-import VideoImage from 'vue-material-design-icons/VideoImage.vue';
-import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
-
+import Magnify from "vue-material-design-icons/Magnify.vue";
+import TelevisionPlay from "vue-material-design-icons/TelevisionPlay.vue";
+import StorefrontOutline from "vue-material-design-icons/StorefrontOutline.vue";
+import AccountGroup from "vue-material-design-icons/AccountGroup.vue";
+import AccountMultiple from "vue-material-design-icons/AccountMultiple.vue";
+import Flag from "vue-material-design-icons/Flag.vue";
+import ClockTimeTwoOutline from "vue-material-design-icons/ClockTimeTwoOutline.vue";
+import Restore from "vue-material-design-icons/Restore.vue";
+import VideoImage from "vue-material-design-icons/VideoImage.vue";
+import DotsHorizontal from "vue-material-design-icons/DotsHorizontal.vue";
 </script>
 
 <template>
-    <div class="posts">
-        Posts
-    </div>
+  <MainNavLayout> Posts </MainNavLayout>
 </template>
