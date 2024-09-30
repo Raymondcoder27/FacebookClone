a0aebf47fabbdb251677afaa3ab9bdf0d373d1b2
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-30 19:41:40 +0300

    adds more posts component functionality

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -3,9 +3,9 @@
     import { useGeneralStore } from '@/stores/general';
 
     import AccountMultiple from 'vue-material-design-icons/AccountMultiple.vue';
-    import ThumbUp from 'vue-material-design-icons/ThumbUp.vue'
-    import Delete from 'vue-material-design-icons/Delete.vue'
-    import Check from 'vue-material-design-icons/Check.vue'
+    import ThumbUp from 'vue-material-design-icons/ThumbUp.vue';
+    import Delete from 'vue-material-design-icons/Delete.vue';
+    import Check from 'vue-material-design-icons/Check.vue';
 
     import { storeToRefs } from 'pinia';
     const useGeneral = useGeneralStore();
@@ -26,8 +26,11 @@
                 <div>
                     <div class="font-extrabold text-[15px]">Raymond Mwebe Dev</div>
                     <div class="flex items-center text-xs text-gray-600">
-                        <AccountMultiple class="ml-1" :size="15" fillColor="#64676B" />
+                      14h  <AccountMultiple class="ml-1" :size="15" fillColor="#64676B" />
                     </div>
+                </div>
+                <div class="flex items-center">
+                    <button class="rounded-full cursor-pointer"></button>
                 </div>
             </div>
         </div>
