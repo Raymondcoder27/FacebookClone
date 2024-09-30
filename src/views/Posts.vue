595e26a5519fc36b59339152894a7c189b16e7d5
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-30 14:01:09 +0300

    adds createPostBox component bugs

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -58,7 +58,7 @@ import DotsHorizontal from "vue-material-design-icons/DotsHorizontal.vue";
             <div id="PostsSection" class="row-span-6 max-w-[600px] lg:mx-0 mx-auto overflow-auto">
                 <CreatePostBox 
                 image="https://picsum.photos/id/140/300/320"
-                placeholder="What's on your mind Raymond." />
+                placeholder="What's on your mind Raymond?!" />
             </div>
 
             <div id="RightSection" class="pl-4 md:block hidden">
