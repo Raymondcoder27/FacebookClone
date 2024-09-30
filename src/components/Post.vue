b0cacbad4eb2606171c514797be7b4afe6e9c897
RaymondCoder27 <mwebraymond13@gmail.com>
2024-09-30 19:28:51 +0300

    adds more posts component functionality

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
     import {toRefs, reactive} from 'vue'
+    import { useGeneralStore } from '@/stores/general';
+
+    import AccountMultiple from 'vue-material-design-icons/AccountMultiple.vue';
+    import ThumbUp from 'vue-material-design-icons/ThumbUp.vue'
+    import Delete from 'vue-material-design-icons/Delete.vue'
+    import Check from 'vue-material-design-icons/Check.vue'
+    import { storeToRefs } from 'pinia';
+    const useGeneral = useGeneralStore();
+
 </script>
 
 <template>
