e716bf3746a4a13bfc0c6af382d77c928c16152a
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 14:14:17 +0300

    fixes post component bugs

---
+++ src/views/User.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import MainNavLayout from '@/Layouts/MainNavLayout.vue';
+    import CreatePostBox from '@/Layouts/CreatePostBox.vue';
+    import Post from '@/components/Post.vue'
+
+    import Camera from 'vue-material-design-icons/Camera.vue';
+    import Pen from 'vue-material-design-icons/Pen.vue';
+
+    import { useGeneralStore } from '@/stores/general';
+    import { storeToRefs } from 'pinia';
+    const useGeneral = useGeneralStore();
+    const {isCropperModal, isImageDisplay} = storeToRefs(useGeneral);
+</script>
+
+<template>
+    
+</template>
