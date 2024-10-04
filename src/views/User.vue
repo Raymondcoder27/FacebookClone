192b3c31df007deb0d7d11e7f112597f302a9483
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-04 14:35:37 +0300

    styles post component

--- src/views/User.vue
+++ src/views/User.vue
@@ -10,8 +10,19 @@
     import { storeToRefs } from 'pinia';
     const useGeneral = useGeneralStore();
     const {isCropperModal, isImageDisplay} = storeToRefs(useGeneral);
+
+    defineProps({posts:Object, user:Object})
 </script>
 
 <template>
-    
+    <Head title="User" />
+    <MainNavLayout>
+        <div class="w-full pb-20">
+            <div class="w-full bg-white">
+                <div class="mx-auto pb-1 pt-[56px] max-w-[1100px]">
+
+                </div>
+            </div>
+        </div>
+    </MainNavLayout>
 </template>
