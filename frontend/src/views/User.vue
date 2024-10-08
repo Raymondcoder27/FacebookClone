2be336209d45d36b7bcb554af2fcac99b62d92e9
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-08 17:08:37 +0300

    fixes User component bugs

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -174,9 +174,9 @@ defineProps({ posts: Object, user: Object });
             <div class="bg-white p-3 mt-4 rounded-lg shadow-lg">
                 <div class="font-extrabold pb-2 text-xl">Photos</div>
                 <div class="flex flex-wrap items-center justify-center w-full">
-                    <div class="w-1/3">
+                    <span class="w-1/3">
                         <img src="https://picsum.photos/id/78/300/300" class="aspect-square object-cover p-1 rounded-lg cursor-pointer">
-                    </div>
+                    </span>
                 </div>
             </div>
           </div>
