9ea70f770ef3d2b4280c9eaf2888cd21309cdd2e
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-08 17:14:01 +0300

    fixes User component bugs

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -182,7 +182,15 @@ defineProps({ posts: Object, user: Object });
           </div>
 
           <div id="PostSection" class="w-full overflow-auto md:w-7/12">
-
+            <CreatePostBox 
+                image="https://picsum.photos/id/140/300/320"
+                placeholder="What's on your mind Raymond?!" />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
           </div>
         </div>
       </div>
