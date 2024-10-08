953ce448e906d1fb235369255ef05cbaf6631934
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-08 17:01:59 +0300

    styles User component

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -173,7 +173,11 @@ defineProps({ posts: Object, user: Object });
 
             <div class="bg-white p-3 mt-4 rounded-lg shadow-lg">
                 <div class="font-extrabold pb-2 text-xl">Photots</div>
-                <div class="flex flex-wrap items-center justify-center w-full"></div>
+                <div class="flex flex-wrap items-center justify-center w-full">
+                    <div class="w-1/3">
+                        <div class="aspect-square object-cover p-1 rounded-lg cursor-pointer"></div>
+                    </div>
+                </div>
             </div>
           </div>
         </div>
