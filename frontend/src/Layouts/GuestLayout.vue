799208ba2558b88e548528ae0c7147fef3a0a120
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-08 19:22:04 +0300

    styles User component

--- frontend/src/Layouts/GuestLayout.vue
+++ frontend/src/Layouts/GuestLayout.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import ApplicationLogo from '@/components/ApplicationLogo.vue';
 
 </script>
 
