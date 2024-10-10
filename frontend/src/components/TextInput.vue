91cfeac4557b651952bd0572cdd915f1086b7582
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-10 12:35:50 +0300

    fixes register component bugs

--- frontend/src/components/TextInput.vue
+++ frontend/src/components/TextInput.vue
@@ -3,6 +3,7 @@
 </script>
 <template>
     <input
-    value="modelValue"
+    :value="modelValue"
+    ref="input"
     @input="$emit('update:modelValue', $event.target.value)" type="text" class="border-gray-300 focus:border-indigo-500 focus:boder-indigo-500 rounded-md shadow-md">
 </template>
