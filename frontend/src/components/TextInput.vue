148303c5863b517942db1f5a174808d29ff7e330
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-10 12:32:05 +0300

    fixes login component bugs

---
+++ frontend/src/components/TextInput.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import { onMounted, ref } from 'vue';
+</script>
+<template>
+    <input
+    value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)" type="text" class="border-gray-300 focus:border-indigo-500 focus:boder-indigo-500 rounded-md shadow-md">
+</template>
