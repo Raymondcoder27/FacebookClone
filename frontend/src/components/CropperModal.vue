4205e7c5a1774f367b85179137a2b3748080dbab
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-11 16:40:11 +0300

    fixes login component bugs

--- frontend/src/components/CropperModal.vue
+++ frontend/src/components/CropperModal.vue
@@ -1,5 +1,14 @@
 <script setup>
+    import {ref} from 'vue'
+    import {useRoute, useRouter} from 'vue-router'
 
+    import {Cropper, CircleStencil} from 'vue-advanced-cropper'
+    import 'vue-advanced-cropper/dist/style.css'
+
+    import Close from 'vue-material-design-icons/Close.vue'
+    import Plus from 'vue-material-design-icons/Plus.vue'
+
+    const emit = defineEmits(['showModal'])
 </script>
 
 <template>
