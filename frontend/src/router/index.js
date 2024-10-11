536df1eabdaa800c04c1ea93e7e73dc34edb4ac5
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-11 13:47:17 +0300

    fixes login component bugs

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -26,6 +26,14 @@ const router = createRouter({
       component: () => import('../views/User.vue')
     },
     {
+      path: '/register',
+      name: 'register',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Register.vue')
+    },
+    {
       path: '/login',
       name: 'login',
       // route level code-splitting
