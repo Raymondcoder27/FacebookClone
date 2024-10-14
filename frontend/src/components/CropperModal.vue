13b4f607bcad69f8f4cd81b30f6361d82fb4f029
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-14 16:55:13 +0300

    adds cropper modal

--- frontend/src/components/CropperModal.vue
+++ frontend/src/components/CropperModal.vue
@@ -69,7 +69,7 @@ const crop = () => {
                   <input type="file" id="image" ref="fileInput" @change="getUploadedImage" class="hidden">
                 </div>
 
-                <div class="w-[350px] mx-auto" v-if="uploadedImage">
+                <div class="w-[350px] mx-auto">
                   <Cropper 
                   class="object-cover"
                   ref="cropper"
