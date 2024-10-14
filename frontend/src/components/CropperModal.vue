a5d91d7b8aa296e35e63b014c86beb5fda32f0d9
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-14 10:39:27 +0300

    fixes routing bugs

--- frontend/src/components/CropperModal.vue
+++ frontend/src/components/CropperModal.vue
@@ -53,6 +53,9 @@ const crop = () => {
               <div class="text-[22px] font-extrabold w-full text-center">
                 Update Profile picture
               </div>
+              <div class="absolute right-3 rounded-full bg-gray-200 hover:bg-gray-300 cursor-pointer p-1.5">
+
+              </div>
             </div>
           </div>
         </div>
