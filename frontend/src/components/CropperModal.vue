26a174d6687a151e9a0e73ce52b7c000e7156d05
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-14 16:33:39 +0300

    fixes cropper modal bugs

--- frontend/src/components/CropperModal.vue
+++ frontend/src/components/CropperModal.vue
@@ -45,8 +45,8 @@ const crop = () => {
 
 <template>
     <div class="fixed z-50">
-      <div class="fixed inset-0 z-40 bg-white bg-opacity-60"></div>
-      <div class="fixed inset-0 z-50 overflow-y-auto">
+      <div class="fixed inset-0 bg-white bg-opacity-60"></div>
+      <div class="fixed inset-0 z-10 overflow-y-auto">
         <div class="flex flex-col min-h-full justify-center items-center py-2">
           <div class="transform overflow-hidden rounded-lg bg-white shadow-2xl transition-all max-w-xl">
             <div class="flex items-center py-4 border-b border-b-gray-300">
