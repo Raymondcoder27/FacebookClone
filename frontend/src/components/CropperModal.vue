7d5dd04e81cefb77d01bbcfa148215e0dbf4677c
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-16 11:32:08 +0300

    fixes cropper modal bugs

--- frontend/src/components/CropperModal.vue
+++ frontend/src/components/CropperModal.vue
@@ -48,7 +48,8 @@ const crop = () => {
       <div class="fixed inset-0 bg-white bg-opacity-60"></div>
       <div class="fixed inset-0 z-10 overflow-y-auto">
         <div class="flex flex-col min-h-full justify-center items-center py-2">
-          <div class="transform overflow-hidden rounded-lg bg-white shadow-2xl transition-all max-w-xl">
+          <!-- <div class="transform overflow-hidden rounded-lg bg-white shadow-2xl transition-all max-w-xl"> -->
+          <div class="transform rounded-lg bg-white shadow-2xl transition-all max-w-xl">
             <div class="flex items-center py-4 border-b border-b-gray-300">
               <div class="text-[22px] font-extrabold w-full text-center">
                 Update Profile picture
