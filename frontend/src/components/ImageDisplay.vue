9ec7e4f08ff342ff9add0fd10d5f03cbc23bc551
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-16 14:24:01 +0300

    fixes cropper modal component bugs

--- frontend/src/components/ImageDisplay.vue
+++ frontend/src/components/ImageDisplay.vue
@@ -12,7 +12,7 @@
         <Close
         @click="isImageDisplay = null" fillColor="#000000" :size="30" class="absolute right-0 p-1 m-3 bg-gray-800 rounded-full cursor-pointer" />
         <div class="grid h-screen place-items-center">
-            <img src="" alt="" class="h-screen p-10 object-scale-down">
+            <img :src="isImageDisplay" alt="" class="h-screen p-10 object-scale-down">
         </div>
     </div>
 </template>
