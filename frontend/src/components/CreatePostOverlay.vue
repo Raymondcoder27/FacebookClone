e9291ca04a43519c0282da8c991f9d47679a6985
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-16 17:16:25 +0300

    fixes cropper modal bugs

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -1,6 +1,15 @@
 <script setup>
     import {ref, reactive} from 'vue';
     import {useRouter} from 'vue-router';
+
+    import VideoImage from 'vue-material-design-icons/VideoImage.vue';
+    import Image from 'vue-material-design-icons/Image.vue';
+    import EmoticonOutline from 'vue-material-design-icons/EmoticonOutline.vue';
+    import Close from 'vue-material-design-icons/Close.vue';
+    import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
+    import Earth from 'vue-material-design-icons/Earth.vue';
+    import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
+    import {useGeneralStore} from '@/stores/general';
 </script>
 
 <template>
