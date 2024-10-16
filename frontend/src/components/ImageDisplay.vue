973411ed378473d97c462e480b1b61cd524a6c00
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-16 13:25:12 +0300

    fixes cropper modal component bugs

--- frontend/src/components/ImageDisplay.vue
+++ frontend/src/components/ImageDisplay.vue
@@ -9,6 +9,7 @@
 
 <template>
     <div id="ImageDisplay" class="fixed z-50 top-0 left-0 w-full h-full bg-black bg-opacity-80">
-        <Close class="absolute right-0 p-1 m-3 bg-gray-800 rounded-full cursor-pointer" />
+        <Close
+        @click="isImageDisplay = null" fillColor="#000000" :size="30" class="absolute right-0 p-1 m-3 bg-gray-800 rounded-full cursor-pointer" />
     </div>
 </template>
