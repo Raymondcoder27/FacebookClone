8220898c6254f32bfebdbddb5c025a30968aef1c
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-17 12:26:11 +0300

    fixes cropper modal component bugs

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -104,9 +104,7 @@
                             </div>
                         </div>
 
-                        <button>
-                            
-                        </button>
+                        <button class="w-full bg-blue-500 hover:bg-blue-600 text-white font-extrabold rounded-lg p-1.5 mt-3"></button>
                     </div>
                 </div>
             </div>
