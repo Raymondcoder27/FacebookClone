016ca369e4609500f784d4b6088010f509ef45e6
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-17 10:11:27 +0300

    fixes cropper modal component bugs

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -38,6 +38,10 @@
 
 <template>
     <div id="CreatePostOverly" class="fixed z-50 top-0 left-0 w-full h-full bg-white bg-opacity-70">
-        
+        <div class="grid h-screen place-items-center p-4">
+            <div class="bg-white w-full mx-auto shadow-xl rounded-xl max-w-[600px]">
+
+            </div>
+        </div>
     </div>
 </template>
