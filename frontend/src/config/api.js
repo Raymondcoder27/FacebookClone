7358268417883e52e4b46d0ffe8f3875572b61c3
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-17 17:06:03 +0300

    adds api.js in config

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,21 @@
-import axios from "axios";
+import axios from 'axios'
 
-const api 
+const api = axios.create({
+    baseURL: import.meta.env.VITE_APP_BASE_URL
+})
+
+api.interceptors.request.use((config) =>{
+    return config
+})
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response != undefined && error.response.status === 401){
+
+        }
+        return Promise.reject(error)
+    }
+)
+
+export default api
