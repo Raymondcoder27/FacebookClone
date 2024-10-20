721a23fb0bf9849cc3ca8d68189cd1dbd2197309
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-20 17:46:31 +0300

    fixes login component bugs

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -12,6 +12,7 @@ import Camera from "vue-material-design-icons/Camera.vue";
 import Pen from "vue-material-design-icons/Pen.vue";
 // import { useRouter } from "vue-router";
 import router from "@/router";
+import {ref} from "vue"
 
 import { useGeneralStore } from "@/stores/general";
 import { storeToRefs } from "pinia";
