f778ff77d49b6404f68acedcf5e43e503edecf1d
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-22 16:48:45 +0300

    tests client connections to server

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
-import { onMounted } from 'vue';
-import { useAuthStore } from '@/stores/auth';
+// import { onMounted } from 'vue';
+// import { useAuthStore } from '@/stores/auth';
 
-const authStore = useAuthStore()
+// const authStore = useAuthStore()
 
-onMounted(() => {
-  // const userDetails = api.get("/validate");
+// onMounted(() => {
+//   // const userDetails = api.get("/validate");
 
-  const userDetails = authStore.validate();
-  // const user = ref(
-  // localStorage.getItem("userDetails") ? JSON.parse(localStorage.getItem("userDetails")) : null;
-  console.log(userDetails);
-});
+//   const userDetails = authStore.validate();
+//   // const user = ref(
+//   // localStorage.getItem("userDetails") ? JSON.parse(localStorage.getItem("userDetails")) : null;
+//   console.log(userDetails);
+// });
 </script>
 
 <template>
