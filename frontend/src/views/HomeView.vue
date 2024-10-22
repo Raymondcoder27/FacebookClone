4e30c9b93f96296419c4a0d5d9b026fb434e37de
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-22 16:47:57 +0300

    fixes client connections to server bugs

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,18 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  // const userDetails = api.get("/validate");
+
+  const userDetails = authStore.validate();
+  // const user = ref(
+  // localStorage.getItem("userDetails") ? JSON.parse(localStorage.getItem("userDetails")) : null;
+  console.log(userDetails);
+});
 </script>
 
 <template>
