4dfd1f7836c602c47a6d6c41d23d5c04cfee142a
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-23 12:51:30 +0300

    fixes post image upload section

--- frontend/src/components/CropperModal.vue
+++ frontend/src/components/CropperModal.vue
@@ -39,7 +39,6 @@ const getUploadedImage = (e) => {
 //         form.image = e.target.files[0];
 //     }
 
-
 // const crop = () => {
 //   const { coordinates, canvas } = cropper.value.getResult()
 //   croppedImageData.imageUrl = canvas.toDataURL()
