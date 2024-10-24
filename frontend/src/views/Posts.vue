454eaa515c1799208ca8893e50ce5f5077c8c41d
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-24 17:58:22 +0300

    fixes backend bugs

--- frontend/src/views/Posts.vue
+++ frontend/src/views/Posts.vue
@@ -57,7 +57,7 @@ onMounted(() => {
                     <router-link to="/user" class="flex items-center justify-start w-full cursor-pointer p-2 hover:bg-[#E5E6E9] rounded-md">
                         <img src="https://picsum.photos/id/87/300/320" class="rounded-full ml-l min-w-[38px] max-h-[38px]">
                         <!-- <div class="text-[15px] text-gray-800 font-extrabold pl-3">Raymond Mwebe</div> -->
-                        <div class="text-[15px] text-gray-800 font-extrabold pl-3">{{userDetails}}</div>
+                        <div class="text-[15px] text-gray-800 font-extrabold pl-3">{{userDetails.name}}</div>
                     </router-link>
                     <button class="flex items-center justify-start w-full cursor-pointer px-2 py-1.5 rounded-md hover:bg-[#E5E6E9]">
                         <AccountMultiple :size="32" fillColor="#5BD7C6" />
