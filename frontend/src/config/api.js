b49f7018e838de621195397ccd1f9355d8cf6736
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-24 17:29:16 +0300

    fixes frontend bugs

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -33,7 +33,7 @@ api.interceptors.response.use(
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('token');
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization = `${token}`;
       console.log("Adding token to headers:", config.headers.Authorization); // Log the header
     }
     return config;
