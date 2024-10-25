d859a19e2fc9b42108312735e2c4f414f78db269
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-25 11:53:20 +0300

    fixes backend bugs

--- frontend/src/Layouts/MainNavLayout.vue
+++ frontend/src/Layouts/MainNavLayout.vue
@@ -189,7 +189,6 @@ onMounted(async () => {
       </button>
       <button
         class="flex items-center p-1 justify-center w-full rounded-lg mx-1 cursor-pointer h-[48px] hover:bg-[#F2F2F2]"
-        @click="getUserDetails"
       >
         <span class="rounded-full p-0.5 border-[2px] border-[#64676B]">
           <AccountGroup class="mx-auto" :size="20" fillColor="#64676B" />
