d1d6d4ec968f204840ee5765d34cc79942a59846
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-25 10:07:18 +0300

    fixes frontend bugs

--- backend/services/imageService.go
+++ backend/services/imageService.go
@@ -15,7 +15,7 @@ import (
 // UpdateImage processes the image upload, deletes the old image if it exists, and saves the new one
 func UpdateImage(model *models.Post, fileHeader *multipart.FileHeader, width, height, left, top int) (*models.Post, error) {
 	// Define the placeholder image path (this can be customized)
-	placeholderImagePath := "./frontend/public/user-placeholder.png"
+	placeholderImagePath := "../frontend/public/user-placeholder.png"
 	publicPath := "public/images/"
 
 	// Check if the model already has an image and delete the old one (if it's not the placeholder)
