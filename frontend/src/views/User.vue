f96878e17a7e443cb0bb1f513038c4ed22770aa2
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-25 12:21:47 +0300

    fixes frontend bugs

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -51,8 +51,13 @@ onMounted(async () => {
     <div class="w-full pb-20 min-h-[100vh] bg-[#F1F2F5]">
       <div class="w-full bg-white">
         <div class="mx-auto pb-1 pt-[56px] max-w-[1100px]">
-          <img
+          <!-- <img
             src="https://picsum.photos/id/45/2000/320"
+            alt=""
+            class="rounded-b-xl"
+          /> -->
+          <img
+            :src="userDetails?.image"
             alt=""
             class="rounded-b-xl"
           />
