421948eea94b2f4a3fc45b22421a3a3b54c062aa
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-25 10:56:25 +0300

    fixes backend bugs

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -11,7 +11,7 @@
     import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
 
     import {defineEmits} from 'vue';
-    import {onMounted, ref, reactive} from 'vue';
+    import {onMounted, reactive} from 'vue';
     import {useAuthStore} from '@/stores/auth';
     import api from '@/config/api';
 
