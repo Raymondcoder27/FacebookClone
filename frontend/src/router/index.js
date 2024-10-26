601cd78cb8c438a43017484b2e33ef1fa61abd4b
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-27 00:55:16 +0300

    tries to fix frontend auth

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from '../views/Login.vue'
 
-
-
 // const appRoutes = [
 //   {
 //     path: "/",
