1221f8700a5bb5fea675e8172be6b45865e5f3c5
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-27 00:49:43 +0300

    tries to fix frontend auth

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -5,61 +5,56 @@ import Post from "@/components/Post.vue";
 import PrimaryButton from "@/components/PrimaryButton.vue";
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import TextInput from "@/components/TextInput.vue";
-// import TextInput from "@"
 import api from "@/config/api";
 import { useAuthStore } from "@/stores/auth";
 
 import Camera from "vue-material-design-icons/Camera.vue";
 import Pen from "vue-material-design-icons/Pen.vue";
-// import { useRouter } from "vue-router";
 import router from "@/router";
-import {ref} from "vue"
+import { ref } from "vue";
 
 import { useGeneralStore } from "@/stores/general";
 import { storeToRefs } from "pinia";
 
-const authStore = useAuthStore()
+const authStore = useAuthStore();
 const useGeneral = useGeneralStore();
 const { isCropperModal, isImageDisplay } = storeToRefs(useGeneral);
-const canResetPassword = true
+const canResetPassword = true;
 
-const email = ref('')
-const password = ref('')
+const email = ref("");
+const password = ref("");
 
 defineProps({ posts: Object, user: Object });
 
-
 const error = ref(null);
 
- const submit = async () => {
-     console.log("logging in...")
+const submit = async () => {
+  console.log("logging in...");
 
-     try {
-         const response = await api.post("/login", {
-             email: email.value,
-             password: password.value,
-         });
-         const token = response.data.token;
-         console.log(token)
+  try {
+    const response = await api.post("/login", {
+      email: email.value,
+      password: password.value,
+    });
+    const token = response.data.token;
+    console.log(token);
 
-         localStorage.setItem('token', token);
+    localStorage.setItem("token", token);
 
+    // localStorage.getItem('token', token)
+    await api.get("/validate", {
+      headers: {
+        Authorization: "Bearer ${token.value}",
+      },
+    });
 
+    // localStorage.getItem('token', token)
 
-         // localStorage.getItem('token', token)
-         await api.get("/validate", {
-         headers: {
-           Authorization: 'Bearer ${token.value}'
-         },
-         });
-
-         // localStorage.getItem('token', token)
-
-         router.push("/home");
-     } catch (error) {
-         error.value = "Login failed. Please check your credentials.";
-     }
- };
+    router.push("/home");
+  } catch (error) {
+    error.value = "Login failed. Please check your credentials.";
+  }
+};
 
 // const submit = async () => {
 //     console.log("logging in...");
@@ -84,7 +79,6 @@ const error = ref(null);
   <!-- <img src="/public/icons/FacebookLogo.png" class="w-[200px]" alt=""> -->
   <div v-if="error" class="text-red-500">{{ error }}</div>
 
-
   <GuestLayout>
     <div
       class="w-full max-h-[50vh] bg-white text-center justify-center items-center mb-5"
@@ -96,7 +90,7 @@ const error = ref(null);
       <form action="" @submit.prevent="submit" class="mb-5 text-black">
         <div>
           <TextInput
-          v-model="email"
+            v-model="email"
             id="email"
             type="email"
             class="mt-1 block w-full"
@@ -112,7 +106,7 @@ const error = ref(null);
 
         <div class="mt-4">
           <TextInput
-          v-model="password"
+            v-model="password"
             id="password"
             type="password"
             class="mt-1 block w-full"
@@ -129,29 +123,29 @@ const error = ref(null);
             to="/home">
               Login
             </RouterLink> -->
-              Login
-           </PrimaryButton>
+            Login
+          </PrimaryButton>
         </div>
 
         <div class="flex items-center justify-center my-5">
           <RouterLink
-          to="/"
-          v-if="canResetPassword"
-          class="hover:underline font-bold text-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            to="/"
+            v-if="canResetPassword"
+            class="hover:underline font-bold text-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
             Forgot Password?
           </RouterLink>
         </div>
 
-
         <div class="border-b border-b-gray-300"></div>
-          <div class="flex items-center justify-center pt-6 pb-2">
-            <RouterLink
+        <div class="flex items-center justify-center pt-6 pb-2">
+          <RouterLink
             to="/register"
-              class="px-5 py-3 text-white bg-[#37A621] text-[20px] font-bold rounded-lg text-sm"
-            >
-              Create a new account.
-            </RouterLink>
-          </div>
+            class="px-5 py-3 text-white bg-[#37A621] text-[20px] font-bold rounded-lg text-sm"
+          >
+            Create a new account.
+          </RouterLink>
+        </div>
       </form>
     </div>
   </GuestLayout>
