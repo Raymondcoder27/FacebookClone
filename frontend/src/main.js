c08fda57ce07f835d66c167fd8bd489bd2c8399f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-27 00:54:17 +0300

    tries to fix frontend auth bugs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,10 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-// import {createInertiaApp} from '@inertiajs/vue3'
-// import {resolvePageComponent} from ''
-// import {ZiggyVue} from '../../vendor/tightenco/ziggy/dist/vue.m'
-
 import App from './App.vue'
 import router from './router'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
