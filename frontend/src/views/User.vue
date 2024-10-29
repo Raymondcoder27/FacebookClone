c785313d3a275583351b6a6808bb05411872b105
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-29 21:17:30 +0300

    fixes login component bugs

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -4,7 +4,6 @@ import CreatePostBox from "@/Layouts/CreatePostBox.vue";
 import Post from "@/components/Post.vue";
 import Camera from "vue-material-design-icons/Camera.vue";
 import Pen from "vue-material-design-icons/Pen.vue";
-
 import { useGeneralStore } from "@/stores/general";
 import { storeToRefs } from "pinia";
 const useGeneral = useGeneralStore();
