7cf8fb128b247b3bd881c49677d13e70c073a4f4
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-29 21:17:24 +0300

    fixes frontend bugs

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -2,7 +2,6 @@
 import MainNavLayout from "@/Layouts/MainNavLayout.vue";
 import CreatePostBox from "@/Layouts/CreatePostBox.vue";
 import Post from "@/components/Post.vue";
-
 import Camera from "vue-material-design-icons/Camera.vue";
 import Pen from "vue-material-design-icons/Pen.vue";
 
