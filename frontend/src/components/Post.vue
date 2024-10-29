73c034c9e8589b155706e9b5195155e802645796
RaymondCoder27 <mwebraymond13@gmail.com>
2024-10-29 21:15:47 +0300

    fixes frontend bugs

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,40 +1,38 @@
 <script setup lang="ts">
-    import {toRefs, reactive} from 'vue'
-    import { useGeneralStore } from '@/stores/general';
+import { toRefs, reactive } from "vue";
+import { useGeneralStore } from "@/stores/general";
 
-    import AccountMultiple from 'vue-material-design-icons/AccountMultiple.vue';
-    import ThumbUp from 'vue-material-design-icons/ThumbUp.vue';
-    import Delete from 'vue-material-design-icons/Delete.vue';
-    import Check from 'vue-material-design-icons/Check.vue';
+import AccountMultiple from "vue-material-design-icons/AccountMultiple.vue";
+import ThumbUp from "vue-material-design-icons/ThumbUp.vue";
+import Delete from "vue-material-design-icons/Delete.vue";
+import Check from "vue-material-design-icons/Check.vue";
 
-    import { storeToRefs } from 'pinia';
-    const useGeneral = useGeneralStore();
+import { storeToRefs } from "pinia";
+const useGeneral = useGeneralStore();
 
-    const {isImageDisplay} = storeToRefs(useGeneral)
+const { isImageDisplay } = storeToRefs(useGeneral);
 
-    const form = reactive({comment: null})
+const form = reactive({ comment: null });
 
+import { useAuthStore } from "@/stores/auth";
+import api from "@/config/api";
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
 
+const userDetails = ref(null);
+const authStore = useAuthStore();
+const router = useRouter();
 
-    import { useAuthStore } from '@/stores/auth';
-    import api from '@/config/api';
-    import { onMounted, ref } from 'vue';
-    import { useRouter } from 'vue-router';
+const post = ref(null);
 
-    const userDetails = ref(null);
-    const authStore = useAuthStore();
-    const router = useRouter();
-
-    const post = ref(null)
-
-    const getPosts = async () => {
-        try {
-            const response = await api.get('/posts')
-            post.value = response.data
-        } catch (error) {
-            console.error('Failed to fetch posts', error)
-        }
-    }
+const getPosts = async () => {
+  try {
+    const response = await api.get("/posts");
+    post.value = response.data;
+  } catch (error) {
+    console.error("Failed to fetch posts", error);
+  }
+};
 
 const getUserDetails = async () => {
   const token = authStore.token;
@@ -42,7 +40,7 @@ const getUserDetails = async () => {
     console.error("No token found");
     return;
   }
-  
+
   try {
     const response = await api.get("/validate", {
       headers: {
@@ -55,7 +53,6 @@ const getUserDetails = async () => {
   }
 };
 
-
 const logout = async () => {
   await authStore.logout();
   router.push("/login");
@@ -66,86 +63,108 @@ onMounted(async () => {
   await getUserDetails();
 
   await getPosts();
-
 });
-
 </script>
 
 <template>
-    <div id="Post" class="w-full bg-white rounded-lg my-4 shadow-md">
-        <div class="flex items-center py-3 px-3">
-            <button class="mr-2">
-                <!-- <img  alt="" class="rounded-full ml-1 min-w-[42px] max-h-[42px]" src="https://picsum.photos/id/140/300/320"> -->
-                <img  alt="" class="rounded-full ml-1 min-w-[42px] max-h-[42px]" :src="userDetails?.image">
-                <!-- <img src="./public/user-placeholder.png" alt="" class="rounded-full ml-1 min-w-[42px] max-h-[42px]"> -->
-            </button>
-            <div class="flex items-center justify-between p-2 rounded-full w-full">
-                <div>
-                    <!-- <div class="font-extrabold text-[15px]">Raymond Mwebe Dev</div> -->
-                    <div class="font-extrabold text-[15px]">{{userDetails?.name}}</div>
-                    <div class="flex items-center text-xs text-gray-600">
-                      <!-- {{ post.CreatedAt }}  -->
-                      14h
-                       <AccountMultiple class="ml-1" :size="15" fillColor="#64676B" />
-                    </div>
-                </div>
-                <div class="flex items-center">
-                    <button class="rounded-full cursor-pointer p-1.5 hover:bg-[#F2F2F2]">
-                        <Delete fillColor="#64676B" />
-                    </button>
-                </div>
-            </div>
+  <div id="Post" class="w-full bg-white rounded-lg my-4 shadow-md">
+    <div class="flex items-center py-3 px-3">
+      <button class="mr-2">
+        <!-- <img  alt="" class="rounded-full ml-1 min-w-[42px] max-h-[42px]" src="https://picsum.photos/id/140/300/320"> -->
+        <img
+          alt=""
+          class="rounded-full ml-1 min-w-[42px] max-h-[42px]"
+          :src="userDetails?.image"
+        />
+        <!-- <img src="./public/user-placeholder.png" alt="" class="rounded-full ml-1 min-w-[42px] max-h-[42px]"> -->
+      </button>
+      <div class="flex items-center justify-between p-2 rounded-full w-full">
+        <div>
+          <!-- <div class="font-extrabold text-[15px]">Raymond Mwebe Dev</div> -->
+          <div class="font-extrabold text-[15px]">{{ userDetails?.name }}</div>
+          <div class="flex items-center text-xs text-gray-600">
+            <!-- {{ post.CreatedAt }}  -->
+            14h
+            <AccountMultiple class="ml-1" :size="15" fillColor="#64676B" />
+          </div>
         </div>
-        <div class="px-5 pb-2 font-semi-bold text-[17px]">
-            <!-- {{post.text}} -->
-             Lorem ipsum dolor sit.
+        <div class="flex items-center">
+          <button class="rounded-full cursor-pointer p-1.5 hover:bg-[#F2F2F2]">
+            <Delete fillColor="#64676B" />
+          </button>
         </div>
-        <img @click="isImageDisplay = 'https://picsum.photos/id/189/800/800'" src="https://picsum.photos/id/189/800/800" alt="" class="mx-auto cursor-pointer">
-        <div id="Likes" class="px-5">
-            <div class="flex items-center justify-between py-3 border-b">
-                <ThumbUp fillColor="#1D72E2" />
-                <div class="text-sm text-gray-600 font-semi-bold">5 comments</div>
-            </div>
-        </div>
-
-        <div id="Comments" class="px-3">
-            <div id="CreateComment" class="flex items-center justify-between py-2">
-                <div class="flex items-center w-full">
-                    <RouterLink to="/">
-                        <img :src="userDetails?.image" alt="" class="rounded-full ml-1 min-w-[36px] max-h-[36px]">
-                    </RouterLink>
-                    <div class="flex items-center justify-center p-2 rounded-full w-full bg-[#EFF2F5]">
-                        <input
-                        v-model="form.comment" 
-                        type="text" 
-                        class="w-full mx-1 border-none p-0 text-sm ring-0 focus:outline-none bg-[#EFF2F5] placeholder-[#64676B]"
-                        placeholder="Write a public comment...">
-                        <button
-                    type="button"
-                    class="flex items-center text-sm pl-2 pr-3.5 rounded-full bg-blue-500 hover:bg-blue-800 text-white font-bold">
-                        <Check fillColor="#FFF" :size="20" /> Send
-                    </button>
-                    </div>
-                </div>
-            </div>
-
-            <div id="Comments" class="max-h-[120px] overflow-auto pb-2 px-4">
-                <div class="flex items-center justify-between pb-2">
-                    <div class="flex items-center w-full mb-1">
-                        <RouterLink to="/">
-                            <img @click="isImageDisplay = 'https://picsum.photos/id/299/800/800'" :src="userDetails?.image" alt="" class="rounded-full min-w-[36px] max-h-[36px]">
-                        </RouterLink>
-                        <div class="flex items-center w-full">
-                            <div class="flex items-center text-xs p-2 rounded-lg w-full bg-[#EFF2F5]">
-                                This is a comment
-                            </div>
-                            <button class="rounded-full ml-2 cursor-pointer">
-                                <Delete fillColor="#64676B" />
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+    <div class="px-5 pb-2 font-semi-bold text-[17px]">
+      <!-- {{post.text}} -->
+      Lorem ipsum dolor sit.
+    </div>
+    <img
+      @click="isImageDisplay = 'https://picsum.photos/id/189/800/800'"
+      src="https://picsum.photos/id/189/800/800"
+      alt=""
+      class="mx-auto cursor-pointer"
+    />
+    <div id="Likes" class="px-5">
+      <div class="flex items-center justify-between py-3 border-b">
+        <ThumbUp fillColor="#1D72E2" />
+        <div class="text-sm text-gray-600 font-semi-bold">5 comments</div>
+      </div>
+    </div>
+
+    <div id="Comments" class="px-3">
+      <div id="CreateComment" class="flex items-center justify-between py-2">
+        <div class="flex items-center w-full">
+          <RouterLink to="/">
+            <img
+              :src="userDetails?.image"
+              alt=""
+              class="rounded-full ml-1 min-w-[36px] max-h-[36px]"
+            />
+          </RouterLink>
+          <div
+            class="flex items-center justify-center p-2 rounded-full w-full bg-[#EFF2F5]"
+          >
+            <input
+              v-model="form.comment"
+              type="text"
+              class="w-full mx-1 border-none p-0 text-sm ring-0 focus:outline-none bg-[#EFF2F5] placeholder-[#64676B]"
+              placeholder="Write a public comment..."
+            />
+            <button
+              type="button"
+              class="flex items-center text-sm pl-2 pr-3.5 rounded-full bg-blue-500 hover:bg-blue-800 text-white font-bold"
+            >
+              <Check fillColor="#FFF" :size="20" /> Send
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div id="Comments" class="max-h-[120px] overflow-auto pb-2 px-4">
+        <div class="flex items-center justify-between pb-2">
+          <div class="flex items-center w-full mb-1">
+            <RouterLink to="/">
+              <img
+                @click="isImageDisplay = 'https://picsum.photos/id/299/800/800'"
+                :src="userDetails?.image"
+                alt=""
+                class="rounded-full min-w-[36px] max-h-[36px]"
+              />
+            </RouterLink>
+            <div class="flex items-center w-full">
+              <div
+                class="flex items-center text-xs p-2 rounded-lg w-full bg-[#EFF2F5]"
+              >
+                This is a comment
+              </div>
+              <button class="rounded-full ml-2 cursor-pointer">
+                <Delete fillColor="#64676B" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
