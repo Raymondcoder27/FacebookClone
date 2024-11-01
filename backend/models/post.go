1224950abde76716187514099208dd76f4774fbf
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-01 19:13:10 +0300

    fixes post rendering

--- backend/models/post.go
+++ backend/models/post.go
@@ -9,6 +9,6 @@ type Post struct {
 	UserID    uint      `json:"user_id"`
 	User      User      `gorm:"foreignKey:UserID" json:"user"`     // Relationship to User
 	Comments  []Comment `gorm:"foreignKey:PostID" json:"comments"` // Relationship to Comments
-	ImageData []byte    `gorm:"type:longblob"`                     // Add 'Image' field to hold image URL
+	ImageData []byte    `gorm:"type:bytea"`                        // Add 'Image' field to hold image URL
 	Text      string    `json:"text"`                              // Add 'Text' field to hold post text
 }
