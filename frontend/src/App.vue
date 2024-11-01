49009ac3814958cc1848785492653ff0ca624e30
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-01 17:41:14 +0300

    fixes post ui rendering

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,64 @@
 import { RouterLink, RouterView } from "vue-router";
 import MainNavLayout from "./Layouts/MainNavLayout.vue";
 import Posts from "./views/Posts.vue";
+
+
+
+
+const posts = ref([]);
+
+const getPosts = async () => {
+  try {
+    const response = await api.get("/posts");
+    posts.value = response.data.data;
+  } catch (error) {
+    console.error("Failed to fetch posts", error);
+  }
+};
+
+const getUserDetails = async () => {
+  const token = authStore.token;
+  if (!token) {
+    console.error("No token found");
+    return;
+  }
+
+  try {
+    const response = await api.get("/validate", {
+      headers: {
+        Authorization: `${token}`,
+      },
+    });
+    userDetails.value = response.data;
+  } catch (error) {
+    console.error("Failed to fetch user details", error);
+  }
+};
+
+// defineProps({
+//   // user: Object,
+//   posts: Object,
+//   // comments: Object,
+// });
+
+defineProps({
+  posts: Array,
+});
+
+const handlePostDeleted = () => {
+  getPosts()
+}
+
+const handlePostAdded = () => {
+  getPosts()
+}
+
+// const { post, user, comments } = toRefs(props);
+
+onMounted(async () => {
+  await getUserDetails();
+  await getPosts();
+});
 </script>
 
 <template>
