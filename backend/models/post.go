9100f1f178b395fc819ef474679ac09917bb7446
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-01 19:05:32 +0300

    fixes post ui rendering

--- backend/models/post.go
+++ backend/models/post.go
@@ -5,10 +5,10 @@ import "gorm.io/gorm"
 // Post model
 type Post struct {
 	gorm.Model
-	Content  string    `json:"content"`                           // Replace 'Email' with 'Content' to hold post content
-	UserID   uint      `json:"user_id"`                           // Foreign key for User
-	User     User      `gorm:"foreignKey:UserID" json:"user"`     // Relationship to User
-	Comments []Comment `gorm:"foreignKey:PostID" json:"comments"` // Relationship to Comments
-	Image    string    `json:"image"`                             // Add 'Image' field to hold image URL
-	Text     string    `json:"text"`                              // Add 'Text' field to hold post text
+	Content   string    `json:"content"`                           // Replace 'Email' with 'Content' to hold post content
+	UserID    uint      `json:"user_id"`                           // Foreign key for User
+	User      User      `gorm:"foreignKey:UserID" json:"user"`     // Relationship to User
+	Comments  []Comment `gorm:"foreignKey:PostID" json:"comments"` // Relationship to Comments
+	ImageData []byte    `gorm:"type:longblob"`                     // Add 'Image' field to hold image URL
+	Text      string    `json:"text"`                              // Add 'Text' field to hold post text
 }
