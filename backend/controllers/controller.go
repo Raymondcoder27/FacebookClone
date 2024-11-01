b86136bc31c77f52519244d2bd4f745fbba6860c
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-01 18:50:25 +0300

    fixes post rendering

--- backend/controllers/controller.go
+++ backend/controllers/controller.go
@@ -210,6 +210,7 @@ func CreatePost(c *gin.Context) {
 	var post models.Post
 	post.UserID = userID.(uint) // Adjust for your authentication setup
 	post.Text = text
+	post.Image = image
 	// post.Image = imagePath // Store the file path
 
 	// Save the post to the database
