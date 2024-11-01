9d64b2738a780e0c9b98cdd0e5b5a2c6810196ad
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-01 17:32:39 +0300

    fixes post ui rendering

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -77,8 +77,8 @@ const createPost = async () => {
         Authorization: `Bearer ${authStore.token}`,
       },
     });
-    emit("postAdded")
-    emit("showModal", false);
+    // emit("postAdded")
+    emit(["showModal", false, "postAdded"]);
   } catch (error) {
     console.error("Failed to create post", error);
     error.value = "Failed to create post. Please try again.";
