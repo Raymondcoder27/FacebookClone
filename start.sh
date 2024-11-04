bb806f258bb0e53adbb08fc129b321ffc05cc440
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-04 11:27:07 +0300

    adds dockerfile

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 if [ -f /usr/local/bin/ .env ]; then
-    export $(grep -v '^#' /usr/local/bin/ .env | xargs)
+    export $(grep -v '^#' /usr/local/bin/.env | xargs)
+
+#Start the backend application
+/usr/local/bin/main &
+
+#Start nginx
+serve -s /usr/share
