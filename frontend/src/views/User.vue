79b887599e6519bffbd47582b61f027f22df017f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-06 17:23:43 +0300

    fixes image upload backend bugs

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -262,7 +262,9 @@ onMounted(() => {
           />
           <!-- Here -->
           <div v-for="post in posts" :key="post.id">
+          <!-- <Post :user="post.user" :post="post" :comments="post.comments" @postDeleted="handlePostDeleted" @postAdded="handlePostAdded" /> -->
           <Post :user="post.user" :post="post" :comments="post.comments" @postDeleted="handlePostDeleted" @postAdded="handlePostAdded" />
+
           </div>
           <!-- <Post />
           <Post />
