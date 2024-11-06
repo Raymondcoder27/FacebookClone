43a0b0eda1b17e952bcb5ee0eea17740373c1fcb
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-06 17:23:55 +0300

    fixes image upload bugs

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -257,13 +257,14 @@ onMounted(() => {
             :placeholder="'What\'s on your mind ' + userDetails?.name + '?!'"
           /> -->
           <CreatePostBox
+          @postAdded="handlePostAdded" 
             :image="userDetails?.image"
             :placeholder="'What\'s on your mind ' + userDetails?.name + '?!'"
           />
           <!-- Here -->
           <div v-for="post in posts" :key="post.id">
           <!-- <Post :user="post.user" :post="post" :comments="post.comments" @postDeleted="handlePostDeleted" @postAdded="handlePostAdded" /> -->
-          <Post :user="post.user" :post="post" :comments="post.comments" @postDeleted="handlePostDeleted" @postAdded="handlePostAdded" />
+          <Post :user="post.user" :post="post" :comments="post.comments" @postDeleted="handlePostDeleted"/>
 
           </div>
           <!-- <Post />
