cb4126ba8dc27dd750e75fd91b267db9c8578691
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-07 18:26:18 +0300

    adds minio to the backend

--- frontend/src/views/Partials/UpdatePasswordForm.vue
+++ frontend/src/views/Partials/UpdatePasswordForm.vue
@@ -3,17 +3,17 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
-// import { useForm } from '@inertiajs/vue3';
+import { useForm } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
 const passwordInput = ref(null);
 const currentPasswordInput = ref(null);
 
-// const form = useForm({
-//     current_password: '',
-//     password: '',
-//     password_confirmation: '',
-// });
+const form = useForm({
+    current_password: '',
+    password: '',
+    password_confirmation: '',
+});
 
 const updatePassword = () => {
     form.put(route('password.update'), {
