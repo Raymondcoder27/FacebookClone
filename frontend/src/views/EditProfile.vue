b4d80889d037c9309b110c605e29ae8ab61b2413
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-07 18:20:46 +0300

    adds minio to the backend

--- frontend/src/views/EditProfile.vue
+++ frontend/src/views/EditProfile.vue
@@ -26,6 +26,14 @@
                     class="max-w-xl"
                     />
                 </div>
+
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <UpdatePasswordForm class="max-w-xl" />
+                </div>
+
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <DeleteUserForm class="max-w-xl" />
+                </div>
             </div>
         </div>
     </MainNavLayout>
