4e70d6e93ee1f64581f63b654cb2963754a068ee
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-07 17:12:34 +0300

    adds minio to the backend

--- backend/services/fileStorage.go
+++ backend/services/fileStorage.go
@@ -1,12 +1,14 @@
 package services
 
 import (
+	"errors"
 	"io"
 
 	"example.com/facebookclone/initializers"
 )
 
-
 func UploadFile(bucketName, objectName string, file io.Reader) error {
-	if initializers.InitMinioClient()
+	if initializers.MinioClient == nil {
+		return errors.New("minio client is not initialised")
+	}
 }
