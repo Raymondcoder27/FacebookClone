528ff6c900e007f0bb6f86e143d4f7500cc22208
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-07 18:10:21 +0300

    adds minio to the backend

--- frontend/src/views/EditProfile.vue
+++ frontend/src/views/EditProfile.vue
@@ -3,7 +3,7 @@
 
     import DeleteUserForm from './Partials/DeleteUserForm.vue';
     import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
-    import UpdateProfileInformation from './Partials/UpdateProfileInformation.vue';
+    import UpdateProfileInformation from './Partials/UpdateProfileInformationForm.vue';
 
     defineProps({
         mustVerifyEmail: Boolean,
@@ -21,6 +21,10 @@
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     <UpdateProfileInformationForm
+                    :must-verify-email="mustVerifyEmail",
+                    :status="status",
+                    class="max-w-xl"
+                    />
                 </div>
             </div>
         </div>
