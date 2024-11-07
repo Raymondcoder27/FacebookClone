38f40747313e791f1d94d81d334db6a0b0832ac6
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-07 17:13:50 +0300

    adds minio to the backend for image storage

--- backend/services/fileStorage.go
+++ backend/services/fileStorage.go
@@ -11,4 +11,9 @@ func UploadFile(bucketName, objectName string, file io.Reader) error {
 	if initializers.MinioClient == nil {
 		return errors.New("minio client is not initialised")
 	}
+
+	//validate bucketname and objectname
+	if bucketName == "" || objectName == "" {
+		return errors.New("bucketName and objectName cannot be empty.")
+	}
 }
