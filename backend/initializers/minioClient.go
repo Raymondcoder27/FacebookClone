921446411365ce99b6ada57857890d6ed4d14e4b
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-07 16:15:29 +0300

    adds minio to the backend for image storage

--- backend/initializers/minioClient.go
+++ backend/initializers/minioClient.go
@@ -1,11 +1,15 @@
 package initializers
 
-import "github.com/minio/minio-go/v7"
+import (
+	"os"
+
+	"github.com/minio/minio-go/v7"
+)
 
 var minioClient *minio.Client
 
 func InitMinioClient() {
-	var err error()
+	var err error
 
 	minioURL := os.Getenv("MINIO_URL")
 }
