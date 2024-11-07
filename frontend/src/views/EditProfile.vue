86427816bf0b481e0ac3e6398619a562bd208ab3
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-07 18:10:32 +0300

    adds Partials for profile editing to front end

--- frontend/src/views/EditProfile.vue
+++ frontend/src/views/EditProfile.vue
@@ -21,8 +21,8 @@
             <div class="max-w-xl mx-auto sm:px-6 lg:px-8 space-y-6">
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     <UpdateProfileInformationForm
-                    :must-verify-email="mustVerifyEmail",
-                    :status="status",
+                    :must-verify-email="mustVerifyEmail"
+                    :status="status"
                     class="max-w-xl"
                     />
                 </div>
