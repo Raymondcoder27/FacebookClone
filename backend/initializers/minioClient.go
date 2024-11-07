6e90f67ff60ccce1ccacdfd01a52b310ab250937
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-07 16:39:35 +0300

    adds minio to the backend

--- backend/initializers/minioClient.go
+++ backend/initializers/minioClient.go
@@ -12,10 +12,10 @@ var minioClient *minio.Client
 
 func InitMinioClient() {
 	var err error
-
 	minioURL := os.Getenv("MINIO_URL")
 	minioAccessKey := os.Getenv("MINIO_ACCESS_KEY")
 	minioSecretKey := os.Getenv("MINIO_SECRET_KEY")
+
 	if minioURL == "" || minioAccessKey = "" || minioSecretKey = "" {
 		log.Fatal("MINIO_URL, MINIO_ACCESS_KEY or MINIO_SECRET_KEY environment variable not set.")
 	}
