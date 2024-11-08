bff27ee936d416d7caf9c89f99d45144ca64ee0f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-08 12:59:37 +0300

    fixes minio

--- frontend/src/views/EditProfile.vue
+++ frontend/src/views/EditProfile.vue
@@ -3,6 +3,7 @@
 
     import DeleteUserForm from './Partials/DeleteUserForm.vue';
     import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
+    // import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
     import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
 
     defineProps({
