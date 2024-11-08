33691ca83a761074c5139d9b4e9e35ab9180e445
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-08 10:44:50 +0300

    fixes minio in backend

--- backend/services/minioDownload.go
+++ backend/services/minioDownload.go
@@ -1,1 +1,8 @@
 package services
+
+
+func DownloadFile(bucketName, objectName string) ([]byte, error) {
+	// Initialize minio client object.
+	client, err := minio.New(
+		os.Getenv("MINIO_HOST"),
+		os.Getenv("MIN
