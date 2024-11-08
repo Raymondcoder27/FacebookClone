3023e1d33e5c7048eb4801678b7e899d3f5f188f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-08 10:45:14 +0300

    fixes minio

--- backend/services/minioDownload.go
+++ backend/services/minioDownload.go
@@ -1,8 +1,15 @@
 package services
 
+import "example.com/facebookclone/initializers"
+
 
 func DownloadFile(bucketName, objectName string) ([]byte, error) {
 	// Initialize minio client object.
-	client, err := minio.New(
-		os.Getenv("MINIO_HOST"),
-		os.Getenv("MIN
+	minioClient := initializers.MinioClient
+
+	//download the object from minio
+	object, err := minioClient.GetObject(bucketName, objectName, minio.GetObjectOptions{})
+	if err != nil {
+		return nil, err
+	}
+	defer object.Close()
