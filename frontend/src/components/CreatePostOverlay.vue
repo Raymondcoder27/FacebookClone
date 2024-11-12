06273cfc9d911e78dcf5ad40c9603727ff460e5d
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-12 12:44:10 +0300

    rectifies file handling during posting in backend

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -102,14 +102,14 @@ const createPost = async () => {
   }
 };
 
-// const getPosts = async () => {
-//   try {
-//     const response = await api.get("/posts");
-//     posts.value = response.data.data;
-//   } catch (error) {
-//     console.error("error fetching posts", error);
-//   }
-// };
+const getPosts = async () => {
+  try {
+    const response = await api.get("/posts");
+    posts.value = response.data.data;
+  } catch (error) {
+    console.error("error fetching posts", error);
+  }
+};
 
 
 
