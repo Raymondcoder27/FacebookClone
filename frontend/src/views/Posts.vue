64be56998bec5b2e1a2788f27ae4e990c66a2b9c
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-13 17:27:06 +0300

    fixes add post functionality

--- frontend/src/views/Posts.vue
+++ frontend/src/views/Posts.vue
@@ -191,11 +191,11 @@ onMounted(() => {
           id="PostsSection"
           class="row-span-6 max-w-[600px] lg:mx-0 mx-auto overflow-auto"
         >
-          <CreatePostBox
+          <!-- <CreatePostBox
           @postAdded="handlePostAdded"
             :image="userDetails?.image"
             :placeholder="'What\'s on your mind ' + userDetails?.name + '?!'"
-          />
+          /> -->
           <CreatePostOverlay v-if="isPostOverlay" @showModal="isPostOverlay = false" @postAdded="handlePostAdded" />
           <!-- <Post /> -->
           <!-- <Post /> -->
