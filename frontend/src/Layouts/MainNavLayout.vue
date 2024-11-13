37a9429a8f77e1d1b671292d513fff01f6e124d7
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-13 13:53:35 +0300

    fixes delete post functionality

--- frontend/src/Layouts/MainNavLayout.vue
+++ frontend/src/Layouts/MainNavLayout.vue
@@ -59,11 +59,24 @@ const logout = async () => {
   router.push("/login");
 };
 
+const handlePostAdded = () => {
+  getPosts()
+}
+
+const getPosts = async () => {
+  try {
+    const response = await api.get("/posts");
+    posts.value = response.data.data;
+  } catch (error) {
+    console.error("Failed to fetch posts", error);
+  }
+};
+
 // Ensure this runs when the component mounts
 onMounted(async () => {
   await getUserDetails();
-
-  await api.get("/posts");
+  getPosts();
+  // await api.get("/posts");
 });
 
 // alert(JSON.stringify(userDetails));
