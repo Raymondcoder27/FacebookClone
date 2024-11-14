7cda6c1952e487b6d3295cf130619b5672ad1706
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-14 10:53:14 +0300

    fixes delete post functionality

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -292,6 +292,8 @@ onMounted(() => {
             :image="userDetails?.image"
             :placeholder="'What\'s on your mind ' + userDetails?.name + '?!'"
           />
+          <CreatePostOverlay v-if="isPostOverlay" @showModal="isPostOverlay = false" @postAdded="handlePostAdded" />
+
 
           <!-- <CreatePostOverlay v-if="isPostOverlay" @showModal="isPostOverlay = false" @postAdded="handlePostAdded" /> -->
           <!-- Here -->
