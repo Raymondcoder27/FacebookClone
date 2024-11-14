344bff7e6c1167b73b71920fed93df5597b28fd4
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-14 17:25:43 +0300

    rectifies file handling during posting

--- frontend/src/Layouts/CreatePostBox.vue
+++ frontend/src/Layouts/CreatePostBox.vue
@@ -30,12 +30,7 @@ const props = defineProps({
   placeholder: String,
 });
 
-// watch(postAdded, (newValue) => {
-//   if (newValue) {
-//     emitPostCreated();
-//     useGeneral.postAdded = false;
-//   }
-// });
+
 
 const { image, placeholder } = toRefs(props);
 </script>
