ef742d45b468923e6e5ed2cbae40d3989ca90f45
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-14 11:57:10 +0300

    fixes add post functionality

--- backend/initializers/loadEnvVars.go
+++ backend/initializers/loadEnvVars.go
@@ -9,7 +9,8 @@ import (
 func LoadEnvVariables() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		// log.Fatal("Error loading .env file")
+		log.Printf("Error loading .env file")
 	}
 	// port := os.Getenv("PORT")
 
