35d6d6c4a914c41cfe3589e76c5cc6a9e785470e
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-14 10:36:33 +0300

    fixes delete post functionality

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -152,15 +152,6 @@ function emitPostCreated() {
   useGeneral.postAdded = true
 }
 
-// const getPosts = async () => {
-//   try {
-//     const response = await api.get("/posts");
-//     posts.value = response.data.data;
-//   } catch (error) {
-//     console.error("error fetching posts", error);
-//   }
-// };
-
 // const handlePostAdded = () => {
 //   getPosts()
 // }
