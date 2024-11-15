597b0f3ff8675753e8436e84a877a32400a89698
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-15 18:36:12 +0300

    reformats frontend files

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -6,7 +6,7 @@ import Camera from "vue-material-design-icons/Camera.vue";
 import Pen from "vue-material-design-icons/Pen.vue";
 import { useGeneralStore } from "@/stores/general";
 import { storeToRefs } from "pinia";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 import { useAuthStore } from "@/stores/auth";
 import api from "@/config/api";
 import CreatePostOverlay from "@/components/CreatePostOverlay.vue";
