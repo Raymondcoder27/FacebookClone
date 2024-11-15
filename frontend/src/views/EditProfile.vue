0fc1f82791767793c90ec51a601eac7a5bd1814f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-15 16:10:13 +0300

    rectifies file handling during posting

--- frontend/src/views/EditProfile.vue
+++ frontend/src/views/EditProfile.vue
@@ -1,41 +1,42 @@
 <script setup>
-    import MainNavLayout from '@/Layouts/MainNavLayout.vue';
+import MainNavLayout from "@/Layouts/MainNavLayout.vue";
 
-    import DeleteUserForm from './Partials/DeleteUserForm.vue';
-    import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
-    // import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
-    // import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
+import DeleteUserForm from "./Partials/DeleteUserForm.vue";
+import UpdatePasswordForm from "./Partials/UpdatePasswordForm.vue";
+// import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
+// import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
 
-    defineProps({
-        mustVerifyEmail: Boolean,
-        status, String,
-    })
+defineProps({
+  mustVerifyEmail: Boolean,
+  status,
+  String,
+});
 </script>
 
 <template>
-    <MainNavLayout >
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>
-        </template>
+  <MainNavLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>
+    </template>
 
-        <div class="py-12 pt-20">
-            <div class="max-w-xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <UpdateProfileInformationForm
-                    :must-verify-email="mustVerifyEmail"
-                    :status="status"
-                    class="max-w-xl"
-                    />
-                </div>
-
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <UpdatePasswordForm class="max-w-xl" />
-                </div>
-
-                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                    <DeleteUserForm class="max-w-xl" />
-                </div>
-            </div>
+    <div class="py-12 pt-20">
+      <div class="max-w-xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+          <UpdateProfileInformationForm
+            :must-verify-email="mustVerifyEmail"
+            :status="status"
+            class="max-w-xl"
+          />
         </div>
-    </MainNavLayout>
+
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+          <UpdatePasswordForm class="max-w-xl" />
+        </div>
+
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+          <DeleteUserForm class="max-w-xl" />
+        </div>
+      </div>
+    </div>
+  </MainNavLayout>
 </template>
