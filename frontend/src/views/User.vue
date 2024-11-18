5d5d68c65643a600c909d42c078971abc52282ee
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-18 08:39:14 +0300

    reformats document frontend files

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -72,13 +72,13 @@ const handlePostAdded = () => {
 };
 
   // Watch for changes to postAdded
-  watch(postAdded, (newValue) => {
-      if (newValue) {
-        getPosts(); // Refetch posts when postAdded is true
-        console.log("watched and fetched in User.vue");
-        postAdded.value = false; // Reset the state after fetching
-      }
-    });
+  // watch(postAdded, (newValue) => {
+  //     if (newValue) {
+  //       getPosts(); // Refetch posts when postAdded is true
+  //       console.log("watched and fetched in User.vue");
+  //       postAdded.value = false; // Reset the state after fetching
+  //     }
+  //   });
 
 onMounted(() => {
   getUserDetails();
