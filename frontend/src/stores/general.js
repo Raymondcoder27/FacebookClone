7e0ee87bee9be307714181b53cc75284092dc006
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-18 09:05:01 +0300

    reformats document frontend files

--- frontend/src/stores/general.js
+++ frontend/src/stores/general.js
@@ -8,7 +8,17 @@ export const useGeneralStore = defineStore('general', () => {
   const isPostOverlay = ref(false)
   const isCropperModal = ref(false)
   const isImageDisplay = ref(null)
-  const postAdded = ref(false)
+  const postAdded = ref(null)
+
+  // Action to emit a post
+  const emitPostAdded = (newPost) => {
+    postAdded.value = newPost; // Update with the new post data
+
+    // Reset the value after emitting so it doesn't stay persistent
+    setTimeout(() => {
+      postAdded.value = null;
+    }, 0);
+  };
 
   return { isPostOverlay, isCropperModal, isImageDisplay, postAdded }
 },
