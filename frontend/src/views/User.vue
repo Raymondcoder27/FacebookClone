64e44b28974b04d2a6f7a1a54c78edc91b1e19f0
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-18 08:49:44 +0300

    reformats document frontend files

--- frontend/src/views/User.vue
+++ frontend/src/views/User.vue
@@ -296,11 +296,11 @@ onMounted(() => {
             :image="userDetails?.image"
             :placeholder="'What\'s on your mind ' + userDetails?.name + '?!'"
           />
-          <CreatePostOverlay
+          <!-- <CreatePostOverlay
             v-if="isPostOverlay"
             @showModal="isPostOverlay = false"
             @postAdded="handlePostAdded"
-          />
+          /> -->
 
           <!-- <CreatePostOverlay v-if="isPostOverlay" @showModal="isPostOverlay = false" @postAdded="handlePostAdded" /> -->
           <!-- Here -->
