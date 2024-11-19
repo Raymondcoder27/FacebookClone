e56cead71afe89dc889e7614551b2045321a918b
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-19 11:31:42 +0300

    reformats frontend files

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -201,7 +201,7 @@ onMounted(async () => {
       >
         <div
           class="flex items-center justify-between pb-2"
-          v-for="comment in comments.comment"
+          v-for="comment in comments"
           :key="comment.ID"
         >
           <div class="flex items-center w-full mb-1">
