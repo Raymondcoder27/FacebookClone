bbc6b365a16dec839802c458dba21db4972c4259
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-19 11:31:27 +0300

    fixes posts call after post addition

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -70,7 +70,7 @@ const CreateComment = async () => {
       text: form.comment,
     });
     comments.value = response.data;
-    alert(JSON.stringify(response));
+    alert(JSON.stringify(response.data));
   } catch (error) {
     console.error("error creating tweet:", error);
   }
