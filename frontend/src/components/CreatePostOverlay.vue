7a318ce535a14f9570d9e637d529e97e6837528b
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-21 17:31:59 +0300

    reformats frontend files

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -50,13 +50,10 @@ const getUploadedImage = (e) => {
   }
 };
 
-// const getUploadedImage = (e) => {
-//   ImageDisplay.value = URL.createObjectURL(e.target.files[0]);
-//   form.image = e.target.files[0];
-
-//   console.log("Uploaded file:", e.target.files[0]);
-//   console.log("Form image value:", form.image);
-// };
+const getUploadedImage = (e) => {
+  uploadedImage.value = URL.createObjectURL(e.target.files[0]);
+  file.value = e.target.files[0];
+};
 
 // const getUploadedImage = (e) => {
 //   if (e.target.files && e.target.files[0]) {
