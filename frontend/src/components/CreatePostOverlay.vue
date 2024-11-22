e11b0368d731ec58fe3d7b607a81c258d4ccae75
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-22 10:48:12 +0300

    fixes posts call after post addition

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -37,7 +37,7 @@ const createPost = async () => {
   }
 
   try {
-    const response = await fetch('/create-post', {
+    const response = await api.post('/create-post', {
       method: 'POST',
       body: formData,
     });
