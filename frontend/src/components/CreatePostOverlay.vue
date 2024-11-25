91f75b8962afa4430356e62a129b428f5bc2e607
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-25 18:01:34 +0300

    reformats frontend files

--- frontend/src/components/CreatePostOverlay.vue
+++ frontend/src/components/CreatePostOverlay.vue
@@ -273,7 +273,7 @@ onMounted(async () => {
                   id="image"
                   type="file"
                   class="hidden"
-                  @change="getUploadedImage($event)"
+                  @input="getUploadedImage($event)"
                 />
                 <button
                   class="hover:bg-gray-200 rounded-full p-2 cursor-pointer"
