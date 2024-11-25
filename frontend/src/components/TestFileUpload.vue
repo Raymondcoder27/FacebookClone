8c1d025c166a7e9b6e3a43bbc929a4e08d396bb1
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-25 13:47:40 +0300

    reformats frontend files

---
+++ frontend/src/components/TestFileUpload.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <input type="file" @change="getUploadedImage" />
+      <img v-if="imageDisplay" :src="imageDisplay" alt="Preview" />
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+  
+  const imageDisplay = ref(null);
+  
+  const getUploadedImage = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      imageDisplay.value = URL.createObjectURL(file); // Generate preview URL
+      console.log("File selected:", file);
+    } else {
+      console.error("No file selected");
+    }
+  };
+  </script>
+  
