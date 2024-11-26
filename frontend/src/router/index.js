85ca93badb05e08311d0aef1b547ff8a79b1aa1f
RaymondCoder27 <mwebraymond13@gmail.com>
2024-11-26 11:05:32 +0300

    fixes posts call after post addition

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -37,15 +37,15 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue'),
       meta: { requiresAuth: true },
     },
-    {
-      path: '/upload',
-      name: 'upload',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../components/TestFileUpload.vue'),
-      meta: { requiresAuth: true },
-    },
+    // {
+    //   path: '/upload',
+    //   name: 'upload',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../components/TestFileUpload.vue'),
+    //   meta: { requiresAuth: true },
+    // },
     {
       path: '/user',
       name: 'user',
